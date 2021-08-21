6541a5afb16b7ca2c6bd0a8398b91deb372311d8
doanyennhi <s3880599@rmit.edu.vn>
2021-08-22 00:12:04 +0700

    Modularize MajorSelectList

---
+++ src/components/MajorSelectList.js
@@ -0,0 +1,21 @@
+const MajorSelectList = () => {
+  const majors = [
+    { code: "BP162", name: "Information Technology" },
+    { code: "BP251", name: "Economics and Finance" },
+    { code: "BP316", name: "Design Studies" },
+    { code: "BP222", name: "Professional Communication" },
+    { code: "BP317", name: "Languages" },
+  ];
+
+  return (
+    <select name="major" id="major">
+      {majors.map(major => (
+        <option key={major.code} value={major.code}>
+          {major.name}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default MajorSelectList;
--- src/pages/ChooseMajor.js
+++ src/pages/ChooseMajor.js
@@ -1,18 +1,12 @@
 import Layout from "../components/Layout";
 import SubjectBoxes from "../components/SubjectBoxes";
+import MajorSelectList from "../components/MajorSelectList";
 
 const ChooseMajor = () => {
   return (
     <Layout>
       <h1>CHOOSE A MAJOR</h1>
-      <select name="major" id="major">
-        <option value="it">Information Technology</option>
-        <option value="ecofin">Economics and Finance</option>
-        <option value="digital-mkt">Digital Marketing</option>
-        <option value="design">Design Studies</option>
-        <option value="profcom">Professional Communication</option>
-        <option value="lang">Languages</option>
-      </select>
+      <MajorSelectList />
 
       <p>WHICH SUBJECTS ARE YOU INTERESTED IN?</p>
       <SubjectBoxes />
