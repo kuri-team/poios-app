20bb24ee808f2179994cf42db60291269d92784c
doanyennhi <s3880599@rmit.edu.vn>
2021-08-22 23:24:51 +0700

    Adjusted margin

--- src/pages/ChooseMajor.js
+++ src/pages/ChooseMajor.js
@@ -2,15 +2,15 @@ import Layout from "../components/Layout";
 import SubjectBoxes from "../components/ChooseMajor/SubjectBoxes";
 import MajorSelectList from "../components/ChooseMajor/MajorSelectList";
 
-import * as majorStyle from "./ChooseMajor.module.css";
+import * as style from "./ChooseMajor.module.css";
 
 const ChooseMajor = () => {
   return (
-    <Layout className={majorStyle["container"]}>
-      <h1 className={majorStyle["h1"]}>Choose a major</h1>
-      <MajorSelectList className={majorStyle["select"]} />
+    <Layout className={style["container"]}>
+      <h1 className={style["h1"]}>Choose a major</h1>
+      <MajorSelectList className={style["select"]} />
 
-      <p>WHICH SUBJECTS ARE YOU INTERESTED IN?</p>
+      <p className={style["subheading"]}>WHICH SUBJECTS ARE YOU INTERESTED IN?</p>
       <SubjectBoxes />
       <button>SET</button>
     </Layout>
