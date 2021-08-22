aa1d54b3da493f814fb7251a2bb365806e7e3214
miketvo <ikxplicitie@gmail.com>
2021-08-22 15:19:33 +0700

    Initial preparation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,8 @@ import * as style from "./Home.module.css";
 
 const Home = () => {
   return (
-    <Layout className={style["container"]} header footer>
+    <Layout className={style["container"]}>
+      <img src={"/media/bg.jpg"} alt="" className={style["background"]} />
       <p>
         Edit <code>src/pages/Home.js</code> and save to hot-reload.
       </p>
