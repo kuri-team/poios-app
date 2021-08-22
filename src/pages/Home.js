96d46a2b29f58cd2c8531d1860bc09be5792c7ca
rider3458 <s3878480@rmit.edu.vn>
2021-08-22 17:30:13 +0700

    add login btn and signup btn

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,9 +5,15 @@ const Home = () => {
   return (
     <Layout className={style["container"]}>
       <img src={"/media/bg.jpg"} alt="" className={style["background"]} />
-      <p>
-        Edit <code>src/pages/Home.js</code> and save to hot-reload.
-      </p>
+      <div className={style["login-signup-box"]}>
+        <div className={style["brand-display"]}>
+          <img src={"/media/Wordmark_Primary.svg"} className={style["brand-name"]} />
+        </div>
+        <div className={style["landing-btn-box"]}>
+          <button className={style["landing-login-btn"] + " " + style["landing-btn"]}>LOGIN</button> <br />
+          <button className={style["landing-signup-btn"] + " " + style["landing-btn"]}>SIGNUP</button>
+        </div>
+      </div>
     </Layout>
   );
 };
