c085ea5b45cf7d8b83bf0cf3d5ddd8f035e69a06
rider3458 <s3878480@rmit.edu.vn>
2021-08-23 17:13:35 +0700

    more styling

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -8,24 +8,25 @@ const Login = () => {
       <img src={"/media/bg.jpg"} alt="" className={commonStyle["background"]} />
       <div className={["login-box"]}>
         <form className={["login-form"]} method={"post"}>
-          <div className={commonStyle["brand-display"]}>
-            <img src={"/media/Wordmark_Primary.svg"} className={commonStyle["brand-name"]} /> <br />
+          <div className={["brand-display"]}>
+            <img src={"/media/Wordmark_Primary.svg"} className={["brand-name"]} /> <br />
             <a href={""} className={"go-back"}>
               Back
             </a>
           </div>
+
           <div className={["login-field-box"]}>
             <div className={["login-field-wrapper"]}>
               <label className={["login-field-label"]}>Username/ Email</label>
               <br />
             </div>
             <div className={["login-field-wrapper"]}>
-              <input className={["login-field-input"]} type={"textarea"} placeholder={"Username/ email"} />
+              <input className={["login-field-input"]} type={"textarea"} />
               <br />
             </div>
             <label className={["login-field-label"]}>Password</label>
             <br />
-            <input className={["login-field-input"]} type={"textarea"} placeholder={"Password"} />
+            <input className={["login-field-input"]} type={"textarea"} />
           </div>
           <div className={["login-btn-box"]}>
             <button className={["login-btn"]}>LOGIN</button> <br />
