ff42249cdcd85ce7e731ba742f15ed3b7772296e
rider3458 <s3878480@rmit.edu.vn>
2021-08-23 11:14:47 +0700

    add more styling and forgot pwd href

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,20 +6,35 @@ const Login = () => {
   return (
     <Layout className={commonStyle["container"] + " " + commonStyle["home"]}>
       <img src={"/media/bg.jpg"} alt="" className={commonStyle["background"]} />
-      <form className={commonStyle["login-box"]} method={"post"}>
-        <div className={style["brand-display"]}>
-          <img src={"/media/Wordmark_Primary.svg"} className={style["brand-name"]} /> <br />
-          <a href={""}>Back</a>
-        </div>
-        <div className={style["login-field"]}>
-          <input className={style["user-field"]} type={"textarea"} placeholder={"Username/ email"} />
-          <br />
-          <input className={style["pwd-field"]} type={"textarea"} placeholder={"Password"} />
-        </div>
-        <div className={style["login-btn-box"]}>
-          <button className={style["login-btn"]}>LOGIN</button> <br />
-        </div>
-      </form>
+      <div className={["login-box"]}>
+        <form className={["login-form"]} method={"post"}>
+          <div className={commonStyle["brand-display"]}>
+            <img src={"/media/Wordmark_Primary.svg"} className={commonStyle["brand-name"]} /> <br />
+            <a href={""} className={"go-back"}>
+              Back
+            </a>
+          </div>
+          <div className={["login-field-box"]}>
+            <div className={["login-field-wrapper"]}>
+              <label className={["login-field-label"]}>Username/ Email</label>
+              <br />
+            </div>
+            <div className={["login-field-wrapper"]}>
+              <input className={["login-field-input"]} type={"textarea"} placeholder={"Username/ email"} />
+              <br />
+            </div>
+            <label className={["login-field-label"]}>Password</label>
+            <br />
+            <input className={["login-field-input"]} type={"textarea"} placeholder={"Password"} />
+          </div>
+          <div className={["login-btn-box"]}>
+            <button className={["login-btn"]}>LOGIN</button> <br />
+            <a href={""} className={"forgot-password"}>
+              Forgot password?
+            </a>
+          </div>
+        </form>
+      </div>
     </Layout>
   );
 };
