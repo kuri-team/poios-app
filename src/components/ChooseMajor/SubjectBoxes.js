0598b7ae4218229f0efb9560d9e25d3cab8ba1d3
doanyennhi <s3880599@rmit.edu.vn>
2021-08-23 23:19:03 +0700

    Implemented function to go to next subject page

--- src/components/ChooseMajor/SubjectBoxes.js
+++ src/components/ChooseMajor/SubjectBoxes.js
@@ -1,9 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import * as style from "./SubjectBoxes.module.css";
 import ToggleButton from "./ToggleButton";
 
 const SubjectBoxes = () => {
   const subjects = [
+    "Omniscient Reader's Viewpoint",
+    "Purple Hyacinth",
+    "Kieran White",
+    "Han Sooyoung",
+    "Kim Dokja",
+    "Yoo Joonghyuk",
+    "Belladonna Davenport",
+    "Lauren Sinclair",
+    "Akashi Seijuro",
     "Introduction to Programming",
     "Prices and Markets",
     "Asian Cinemas",
@@ -16,27 +25,41 @@ const SubjectBoxes = () => {
     "Photography 101",
     "Introduction to Language",
     "Introduction to Fashion Enterprise",
+    "Accounting in Organisations & Society",
+    "Web Programming",
+    "Commercial Law",
+    "Business Statistics 1",
   ];
 
-  const [currentPage, setCurrentPage] = useState(1);
-  const [subjectList, setSubjectList] = useState(subjects);
-
   const MAX_BOX_PER_PAGE = 12;
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [subjectList, setSubjectList] = useState(subjects.slice(0, MAX_BOX_PER_PAGE));
+
   let numPage =
     subjects.length % MAX_BOX_PER_PAGE === 0
       ? subjects.length / MAX_BOX_PER_PAGE
-      : subjects.length / MAX_BOX_PER_PAGE + 1;
+      : Math.ceil(subjects.length / MAX_BOX_PER_PAGE);
 
   const togglePrevPage = event => {
     if (currentPage <= 1) {
-      console.log("Page 1");
       event.preventDefault();
     } else {
       setCurrentPage(currentPage - 1);
-      setSubjectList(subjects.slice((currentPage - 1) * MAX_BOX_PER_PAGE, currentPage * MAX_BOX_PER_PAGE));
-      console.log(`page ${currentPage}`);
     }
   };
+
+  const toggleNextPage = event => {
+    if (currentPage >= numPage) {
+      event.preventDefault();
+    } else {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  useEffect(() => {
+    setSubjectList(subjects.slice((currentPage - 1) * MAX_BOX_PER_PAGE, currentPage * MAX_BOX_PER_PAGE));
+  }, [currentPage]);
 
   // const [selectedIds, setSelectedIds] = useState([]);
   //
@@ -53,13 +76,13 @@ const SubjectBoxes = () => {
       <div className={style["prev-btn"]}>
         <ToggleButton src={"/media/chevron-left.svg.png"} onClick={event => togglePrevPage(event)} />
       </div>
-      {subjects.map((subject, id) => (
+      {subjectList.map((subject, id) => (
         <div key={id} className={style["subject-box"]}>
           {subject}
         </div>
       ))}
       <div className={style["next-btn"]}>
-        <ToggleButton src={"/media/chevron-right.svg.png"} />
+        <ToggleButton src={"/media/chevron-right.svg.png"} onClick={event => toggleNextPage(event)} />
       </div>
     </div>
   );
