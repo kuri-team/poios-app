6093000a265c49bf7b54fc718b10720fcb5e4bf9
Vu Bui Khanh Linh <s3864120@rmit.edu.vn>
2021-08-24 21:33:35 +0700

    Fix SearchBar responsiveness

--- src/components/TutorList/Searchbar.js
+++ src/components/TutorList/Searchbar.js
@@ -2,28 +2,28 @@ import * as style from "./Searchbar.module.css";
 
 const Searchbar = ({ searchQuery, setSearchQuery }) => {
   return (
-    <>
-      <div className={style["search-bigcontainer"]}>
-        <div className={style["search-container"]}>
-          <span className={style["Icon"]}>
-            <img className={style["searchIcon"]} src={"media/icons/search_black_24dp.svg"} alt="" />
-            <img className={style["filterIcon"]} src={"media/icons/filter.svg"} alt="" />
-          </span>
+    <div className={style["search-bigcontainer"]}>
+      <div className={style["search-container"]}>
+        <span className={style["search-icon-container"]}>
+          <img className={style["searchIcon"]} src={"media/icons/search_black_24dp.svg"} alt="" />
+        </span>
+        <span className={style["filter-icon-container"]}>
+          <img className={style["filterIcon"]} src={"media/icons/filter.svg"} alt="" />
+        </span>
 
-          <form action="/" method="get">
-            <input
-              className={style["form-control"]}
-              value={searchQuery}
-              onInput={e => setSearchQuery(e.target.value)}
-              type="text"
-              id="tutor-search"
-              placeholder="Search for tutors"
-              name="search-result"
-            />
-          </form>
-        </div>
+        <form action="/" method="get">
+          <input
+            className={style["form-control"]}
+            value={searchQuery}
+            onInput={e => setSearchQuery(e.target.value)}
+            type="text"
+            id="tutor-search"
+            placeholder="Search for tutors"
+            name="search-result"
+          />
+        </form>
       </div>
-    </>
+    </div>
   );
 };
 
