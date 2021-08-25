7f7c7d20bd788541bf2e0e69ef3d236b6655de83
doanyennhi <s3880599@rmit.edu.vn>
2021-08-25 10:14:29 +0700

    Fixed key prop warning

--- src/components/ChooseMajor/SubjectBox.js
+++ src/components/ChooseMajor/SubjectBox.js
@@ -2,12 +2,11 @@ import { useState } from "react";
 
 import * as style from "./SubjectBox.module.css";
 
-const SubjectBox = ({ key, subject, styleObject }) => {
+const SubjectBox = ({ subject, styleObject }) => {
   const [selected, setSelected] = useState(false);
 
   return (
     <div
-      key={key}
       className={selected ? style["subject-box-selected"] : style["subject-box"]}
       style={styleObject}
       onClick={() => setSelected(!selected)}
