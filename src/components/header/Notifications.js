745ac496aa80c37d67487a100a9a0558dd63205f
miketvo <ikxplicitie@gmail.com>
2021-08-25 21:29:18 +0700

    More styles for dropdown menu

--- src/components/header/Notifications.js
+++ src/components/header/Notifications.js
@@ -1,6 +1,34 @@
 import * as commonStyle from "./common.module.css";
+import { Link } from "react-router-dom";
 
 const Notifications = ({ active }) => {
+  const notifications = [
+    {
+      title: "Request Accepted",
+      description: "Lacnham accepted your tutor request",
+    },
+    {
+      title: "Request Declined",
+      description: "miketvo declined your tutor request",
+    },
+    {
+      title: "Request Accepted",
+      description: "Lacnham accepted your tutor request",
+    },
+    {
+      title: "Request Declined",
+      description: "miketvo declined your tutor request",
+    },
+    {
+      title: "Request Accepted",
+      description: "Lacnham accepted your tutor request",
+    },
+    {
+      title: "Request Declined",
+      description: "miketvo declined your tutor request",
+    },
+  ];
+
   return (
     <div className={commonStyle["wrapper"]}>
       <button className={commonStyle["button"]}>
@@ -11,6 +39,22 @@ const Notifications = ({ active }) => {
           <img src={"/media/notifications_black_24dp.svg"} alt={"Notifications"} />
         )}
       </button>
+      <div className={commonStyle["menu"]}>
+        <div className={commonStyle["header"]}>
+          <h2>Notifications</h2>
+        </div>
+        <div className={commonStyle["content"]}>
+          {notifications.map(notification => (
+            <div className={commonStyle["item"]}>
+              <h2>{notification.title}</h2>
+              <p>{notification.description}</p>
+            </div>
+          ))}
+        </div>
+        <div className={commonStyle["footer"]}>
+          <Link to="/">All notifications</Link>
+        </div>
+      </div>
     </div>
   );
 };
