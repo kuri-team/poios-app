6890e41462b0d8fee3cec08e7b5d60d84d60004a
Vu Bui Khanh Linh <s3864120@rmit.edu.vn>
2021-08-25 18:30:50 +0700

    Add FilterWindow frame; Toggle FilterWindow function; "Search function" in FilterWindow

--- src/components/TutorList/Searchbar.js
+++ src/components/TutorList/Searchbar.js
@@ -1,29 +1,34 @@
 import * as style from "./Searchbar.module.css";
+import { useState } from "react";
 
-const Searchbar = ({ searchQuery, setSearchQuery }) => {
+const Searchbar = ({ searchQuery, setSearchQuery, displayState }) => {
+  const [showFilter, setShowFilter] = useState(false);
+
   return (
-    <div className={style["search-bigcontainer"]}>
-      <div className={style["search-container"]}>
-        <span className={style["search-icon-container"]}>
-          <img className={style["searchIcon"]} src={"media/icons/search_black_24dp.svg"} alt="" />
-        </span>
-        <span className={style["filter-icon-container"]}>
-          <img className={style["filterIcon"]} src={"media/icons/filter.svg"} alt="" />
-        </span>
+    <>
+      <div className={style["search-bigcontainer"]}>
+        <div className={style["search-container"]}>
+          <span className={style["search-icon-container"]}>
+            <img className={style["searchIcon"]} src={"media/icons/search_black_24dp.svg"} alt="" />
+          </span>
+          <span className={style["filter-icon-container"]} onClick={displayState}>
+            <img className={style["filterIcon"]} src={"media/icons/filter.svg"} alt="" />
+          </span>
 
-        <form action="/" method="get">
-          <input
-            className={style["form-control"]}
-            value={searchQuery}
-            onInput={e => setSearchQuery(e.target.value)}
-            type="text"
-            id="tutor-search"
-            placeholder="Search for tutors"
-            name="search-result"
-          />
-        </form>
+          <form action="/" method="get">
+            <input
+              className={style["form-control"]}
+              value={searchQuery}
+              onInput={e => setSearchQuery(e.target.value)}
+              type="text"
+              id="tutor-search"
+              placeholder="Search for tutors"
+              name="search-result"
+            />
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
