401c7eff584f539db1552f71ef853208263a1ade
Vu Bui Khanh Linh <s3864120@rmit.edu.vn>
2021-08-26 13:14:13 +0700

    -Add Reviews to TutorProfile.js

--- src/pages/TutorProfile.js
+++ src/pages/TutorProfile.js
@@ -1,6 +1,7 @@
 import Layout from "../components/Layout";
 import TutorProfileInfo from "../components/Profile/TutorProfileInfo";
 import Timetable from "../components/Profile/Timetable";
+import Reviewer from "../components/Profile/Reviewer";
 
 import * as style from "./TutorProfile.module.css";
 import OwnProfileInfo from "../components/Profile/OwnProfileInfo";
@@ -11,6 +12,9 @@ const TutorProfile = () => {
       <div className={style["tutorInfo-center-container"]}>
         <h2 className={style["title-header"]}>Profile</h2>
         <TutorProfileInfo />
+      </div>
+      <div className={style["reviewer-center-container"]}>
+        <Reviewer />
       </div>
       <div className={style["timetable-center-container"]}>
         <h2 className={style["title-header"]}>TimeTable</h2>
