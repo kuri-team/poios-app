a745b125d22941ad0ab421478d9874bfe9cf1b1c
Hoang Linh <s3880313@rmit.edu.vn>
2021-08-26 15:07:48 +0700

    reformat from-me chat float to right

--- src/components/Chatroom/ChatWindow.js
+++ src/components/Chatroom/ChatWindow.js
@@ -7,14 +7,21 @@ const ChatWindow = ({ roomName }) => {
   const messages = [
     {
       fromMe: true,
-      sender: "hlinh_pr0_vjp",
+      sender: "hlinh_santa_claus",
       photo: "/media/chatroom-icons/hacker.svg",
       content:
-        "Hello World! Hello World! Hello World! Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!",
+        "Welcome to Mars! Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
       timestamp: "2021",
     },
     {
       fromMe: false,
+      sender: "hlinh_pr0_vjp",
+      photo: "/media/chatroom-icons/hacker.svg",
+      content: "Hello World!",
+      timestamp: "2021",
+    },
+    {
+      fromMe: true,
       sender: "hlinh_pr0_vjp",
       photo: "/media/chatroom-icons/hacker.svg",
       content: "Hello World!",
