bbb09c61df9a4f8994e7fc5024f63906c2134bab
miketvo <ikxplicitie@gmail.com>
2021-08-29 22:37:03 +0700

    Removed sidebar because we don't need it anymore

--- src/components/Chat/ChatWindow.js
+++ src/components/Chat/ChatWindow.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import Message from "./Message";
 import * as style from "./ChatWindow.module.css";
 
-const ChatWindow = ({ roomName }) => {
+const ChatWindow = ({ tutorName }) => {
   const messages = [
     {
       fromMe: true,
@@ -14,7 +14,7 @@ const ChatWindow = ({ roomName }) => {
     },
     {
       fromMe: false,
-      sender: "Mr.Robot",
+      sender: "Mr. Robot",
       photo: "/media/chatroom-icons/hacker.svg",
       content: "Hello World!",
       timestamp: "2021",
@@ -26,71 +26,119 @@ const ChatWindow = ({ roomName }) => {
       content: "Hello World!",
       timestamp: "2021",
     },
+    {
+      fromMe: false,
+      sender: "Mr. Robot",
+      photo: "/media/chatroom-icons/hacker.svg",
+      content: "Hello World!",
+      timestamp: "2021",
+    },
+    {
+      fromMe: true,
+      sender: "Anonymous",
+      photo: "/media/chatroom-icons/hacker.svg",
+      content: "Welcome to Mars!",
+      timestamp: "2021",
+    },
+    {
+      fromMe: false,
+      sender: "Mr. Robot",
+      photo: "/media/chatroom-icons/hacker.svg",
+      content: "Hello World!",
+      timestamp: "2021",
+    },
+    {
+      fromMe: true,
+      sender: "Anonymous",
+      photo: "/media/chatroom-icons/hacker.svg",
+      content: "Hello World!",
+      timestamp: "2021",
+    },
+    {
+      fromMe: false,
+      sender: "Mr. Robot",
+      photo: "/media/chatroom-icons/hacker.svg",
+      content: "Hello World!",
+      timestamp: "2021",
+    },
   ];
 
   return (
     <div className={style["chat-window"]}>
       <div className={style["chat-window-header"]}>
-        {/*<h1 className={style["header-title"]}>{roomName}</h1>*/}
-        <div className={style["profile-wrapper"]}>
+        <div className={style["tutor-wrapper"]}>
           <Link to="/">
-            <img className={style["profile-avatar"]} src="/media/chatroom-icons/hacker.svg" alt="avatar" />
+            <img className={style["tutor-avatar"]} src={"/media/chatroom-icons/hacker.svg"} alt="avatar" />
           </Link>
-          <Link to="/profile/my-profile" className={style["profile-username"]}>
-            Username
+          <Link to="/profile" className={style["tutor-name"]}>
+            {tutorName}
           </Link>
         </div>
 
         <div className={style["header-button-group"]}>
           <button className={style["leave-button"]}>Leave</button>
-          <div className={style["function-button"]}>
-            <img className={style["function-icon"]} src={"/media/chatroom-icons/chat.svg"} alt="Chat icon" />
+          <div className={style["functions"]}>
+            <div className={style["function-button"]}>
+              <img
+                className={style["function-icon"]}
+                src={"/media/chatroom-icons/chat_24dp.svg"}
+                alt="Messages"
+                style={{ width: "22px", height: "22px", transform: "translateY(1px)" }}
+              />
+            </div>
+            <div className={style["function-button"]}>
+              <img
+                className={style["function-icon"]}
+                src={"/media/chatroom-icons/screen_share_24dp.svg"}
+                alt="Screen share"
+              />
+            </div>
+            <div className={style["function-button"]}>
+              <img
+                className={style["function-icon"]}
+                src={"/media/chatroom-icons/video_call_24dp.svg"}
+                alt="Video call"
+                style={{ width: "30px", height: "30px", transform: "translateX(1px)" }}
+              />
+            </div>
+            <div className={style["function-button"]}>
+              <img className={style["function-icon"]} src={"/media/chatroom-icons/call_24dp.svg"} alt="Voice call" />
+            </div>
+            <button className={style["mobile-menu-button"]}>
+              <img
+                className={style["function-icon"]}
+                src={"/media/chatroom-icons/hamburger.svg"}
+                alt="Hamburger icon"
+              />
+            </button>
           </div>
-          <div className={style["function-button"]}>
-            <img className={style["function-icon"]} src={"/media/chatroom-icons/screen.svg"} alt="Screen share icon" />
-          </div>
-          <div className={style["function-button"]}>
-            <img
-              className={style["function-icon"]}
-              src={"/media/chatroom-icons/video-camera.svg"}
-              alt="Video call icon"
-            />
-          </div>
-          <div className={style["function-button"]}>
-            <img className={style["function-icon"]} src={"/media/chatroom-icons/call.svg"} alt="Voice call icon" />
-          </div>
-          <Link to="/" className={style["mobile-menu-button"]}>
-            <img className={style["function-icon"]} src={"/media/chatroom-icons/hamburger.svg"} alt="Hamburger icon" />
-          </Link>
         </div>
       </div>
 
       <div className={style["chat-window-content"]}>
-        <div className={style["wrapper"]}>
-          {messages.map(message => (
-            <Message
-              fromMe={message.fromMe}
-              sender={message.sender}
-              photo={message.photo}
-              content={message.content}
-              timestamp={message.timestamp}
-            />
-          ))}
-        </div>
+        {messages.map(message => (
+          <Message
+            fromMe={message.fromMe}
+            sender={message.sender}
+            photo={message.photo}
+            content={message.content}
+            timestamp={message.timestamp}
+          />
+        ))}
       </div>
 
       <div className={style["form"]}>
         <div className={style["attach-button"]}>
-          <img className={style["function-icon"]} src={"/media/chatroom-icons/photo.svg"} alt="Send photo icon" />
+          <img className={style["function-icon"]} src={"/media/chatroom-icons/photo.svg"} alt="Attach a photo" />
         </div>
         <div className={style["attach-button"]}>
-          <img className={style["function-icon"]} src={"/media/chatroom-icons/attachment.svg"} alt="Send file icon" />
+          <img className={style["function-icon"]} src={"/media/chatroom-icons/attachment.svg"} alt="Attach a file" />
         </div>
         <div className={style["input-message-content"]}>
           <input type="text" className={style["input-message"]} name="input-message" placeholder="Write a message..." />
         </div>
         <div className={style["send-button"]}>
-          <img className={style["function-icon"]} src={"/media/chatroom-icons/send.svg"} alt="Send icon" />
+          <img className={style["function-icon"]} src={"/media/chatroom-icons/send_24dp.svg"} alt="Send" />
         </div>
       </div>
     </div>
