e6abea7de814b6ac4cfc88f7200f57fe1633ef6a
doanyennhi <s3880599@rmit.edu.vn>
2021-08-30 23:38:03 +0700

    Reuse elements and styling from TutorProfileInfo.js, clean up & remove unnecessary code

--- src/components/Tutors/TutorListElement.js
+++ src/components/Tutors/TutorListElement.js
@@ -2,18 +2,36 @@ import * as style from "./TutorListElement.module.css";
 
 const TutorListElement = () => {
   return (
-    <>
-      <div className={style["tutor-element"]}>
-        <div className={style["avatar"]}>
-          <img src={"/media/venti.jpg"} alt="Tutor" />
+    <div className={style["profile-big-container"]}>
+      <div className={style["profile-container"]}>
+        <div className={style["user-avatar"]}>
+          <img className={style["avatar-image"]} src={"/media/venti.jpg"} alt="" />
         </div>
 
-        <div className={style["avatar-name"]}>
-          <h3>Venti</h3>
+        <div className={style["user-info-display"]}>
+          <div>
+            <h2>Venti</h2>
+            <p>Tutor</p>
+            <p>Major Musical Instruments</p>
+          </div>
+
+          <div>
+            <div className={style["user-info-element"]}>
+              Subject:
+              <div className={style["subject-list"]}>
+                <div>JavaScript</div>
+                <div>ReactJS</div>
+                <div>NoSQL</div>
+                <div>PHP</div>
+                <div>PHP</div>
+                <div>PHP</div>
+                <div>PHP</div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className={style["avatar-subject"]}>Tone-Deaf Bard</div>
       </div>
-    </>
+    </div>
   );
 };
 
