6ddba3b69874711fc55956d0ddbccc509cdc3767
doanyennhi <s3880599@rmit.edu.vn>
2021-09-09 22:36:07 +0700

    Resolve select default value warning

--- src/components/FieldsOfStudy/MajorSelectMenu.js
+++ src/components/FieldsOfStudy/MajorSelectMenu.js
@@ -4,6 +4,7 @@ const MajorSelectMenu = ({ majors, callback }) => {
   return (
     <div className={style["wrapper"]}>
       <select
+        defaultValue=""
         className={style["select"]}
         name="major"
         id="major"
@@ -11,9 +12,7 @@ const MajorSelectMenu = ({ majors, callback }) => {
           callback(e.target.value);
         }}
       >
-        <option value="" selected>
-          Please select a major
-        </option>
+        <option value="">Please select a major</option>
         {majors.map(major => (
           <option key={major._id} value={major.code}>
             {major.name}
