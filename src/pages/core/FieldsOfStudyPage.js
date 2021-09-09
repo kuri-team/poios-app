27b08052a910c9a4bd365637938b4668d0d0b572
doanyennhi <s3880599@rmit.edu.vn>
2021-09-09 17:25:51 +0700

    Fetch and pass data to SubjectBoxes.js

--- src/pages/core/FieldsOfStudyPage.js
+++ src/pages/core/FieldsOfStudyPage.js
@@ -1,5 +1,6 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link, Redirect } from "react-router-dom";
+import axios from "axios";
 
 import { GlobalState } from "../../GlobalState";
 import Layout from "../../components/Layout";
@@ -13,6 +14,14 @@ const FieldsOfStudyPage = () => {
   console.log(state);
   const [isLogged, setIsLogged] = state.userApi.isLogged;
   const [isTutor, setIsTutor] = state.userApi.isTuTor;
+  const [subjects, setSubjects] = useState([]);
+
+  const getFields = () => axios.get("/core/fields-of-study").then(res => console.log(res.data));
+
+  useEffect(() => {
+    getFields();
+    console.log(subjects);
+  }, []);
 
   const tutorRouter = () => {
     return (
@@ -27,7 +36,7 @@ const FieldsOfStudyPage = () => {
       <MajorSelectMenu />
 
       <p className={style["subheading"]}>WHICH SUBJECTS ARE YOU INTERESTED IN?</p>
-      <SubjectBoxes />
+      <SubjectBoxes subjects={subjects} />
       {isTutor ? tutorRouter() : <button className={style["btn"]}>SET</button>}
     </Layout>
   );
